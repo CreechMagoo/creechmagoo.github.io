7cf2a9a4d51fa7ad6ccf8bbcfeeeb0b3c0f682e0
Boba <trashboyjet@gmail.com>
2025-01-15 15:40:55 -0600

    404 page + bug fixes

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,8 @@ const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 document.addEventListener("DOMContentLoaded", (event) => {
 	document.getElementById("copyright").innerHTML = '© 2024-'+year+' SCUM DOG LTD</span>';
 	
-	// mobile check
-	if (isMobile) {
+	// force game to be played in fullscreen on mobile
+	if (isMobile && document.body.contains(document.getElementById("gameContainer"))) {
 		document.getElementById("gameContainer").outerHTML = "<a href=\""+document.getElementById("game").src+"\">PLAY NOW</a>";
 	}
 });
